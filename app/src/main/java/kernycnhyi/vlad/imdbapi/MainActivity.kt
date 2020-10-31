26d59b373726e856686d3c2c1186b35c3ed22145
Vladislav Kernychnyi <bastard290@gmail.com>
2020-10-31 15:02:53 +0200

    Init Commit

---
+++ app/src/main/java/kernycnhyi/vlad/imdbapi/MainActivity.kt
@@ -0,0 +1,12 @@
+package kernycnhyi.vlad.imdbapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
